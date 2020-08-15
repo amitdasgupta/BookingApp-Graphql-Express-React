9181736a8f3f6e7df0f443145c146d93cb4971d8
amitdasgupta <amitdsgupta007@gmail.com>
2020-08-15 23:54:10 +0530

    Made changes for deployment

--- server/app.js
+++ server/app.js
@@ -42,11 +42,12 @@ app.use(
   })
 );
 
-// app.use(express.static(path.join(__dirname, "../public", "build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../public", "build", "index.html"));
-// });
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 4000;
 
